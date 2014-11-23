65e4ef519c92cce76063595ad32b48d4ccc9eebe
Sammy Teahan <sam.teahan@aggiemail.usu.edu>
2014-11-23 16:52:34 -0700

    Adding new db schema and hashing users' passwords, integrating flask wtforms for the login form, still need to run check_hash method to compare password with db value and login user from the database

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from flask import render_template
 from functools import wraps
 from flask import request, url_for, redirect, session, g, flash
 from flask_wtf import Form
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, TextField, PasswordField
 from wtforms.validators import DataRequired, Required
+from flask.ext.mongokit import MongoKit
 from mongokit import Connection, Document
+from flask.ext.bcrypt import Bcrypt
 import os
 
 ### DB configuration
@@ -14,23 +16,38 @@ MONGODB_PORT = 27017
 
 ### Our applications name:
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
+db = MongoKit(app)
 
 ### Connect to a local DB called `fighters`
+### and insert an admin user
 connection = Connection()
-testCollection = connection['fighters'].fighters
-user = {'name': u'admin', 'email': u'admin@localhost'}
-# testCollection.insert(user)
+fighterCollection = connection['fighters'].fighters
+password = bcrypt.generate_password_hash('meo123')
+admin = {'name': u'admin', 'password': password}
+fighterCollection.insert(admin)
 
+@db.register
+class Fighter(Document):
+	__collection__ = 'fighters'
+	structure = {
+		'name': unicode,
+		'password': unicode,
+	}
+	required_fields = ['name', 'password']
+	use_dot_notation = True
 
-# for development
-user = 'sammy'
-password = '123qwe'
-
-app.config['SECRET_KEY'] = 	'{E]\x1eo\xa3\x04w\xe2\x1b\xf7|\x97\x93N8\x7f\xa4\xf0\xe5\n\xa7\xfc\x02' # .config is a dictionary used by flask-wtf 
+# config is a dictionary used by flask-wtf 
+app.config['SECRET_KEY'] = '{E]\x1eo\xa3\x04w\xe2\x1b\xf7|\x97\x93N8\x7f\xa4\xf0\xe5\n\xa7\xfc\x02' 
 
 class NameForm(Form):
 	name = StringField('Name:', validators=[Required()])
 	submit = SubmitField('Submit')
+
+
+class LoginForm(Form):
+	username = TextField('username', validators=[Required()])
+	password = PasswordField('password', validators=[Required()])
 
 # login_required decorator to ensure a user is logged in
 def login_required(function):
@@ -42,24 +59,28 @@ def login_required(function):
 			return redirect (url_for('login'))
 	return wrap
 
-
 @app.route('/', methods=["GET", "POST"])
 def login():
 	name = None
 	error = ''
+	form = LoginForm(request.form)
 
-	if request.method == "POST":
-		if request.form['user'] == user and request.form['password'] == password:
-			session['username'] = request.form['user']
+	if request.method == 'POST':
+		if form.validate_on_submit():
+			print 'in here yo'
+			fighter = db.Fighter()
+			fighter.name = request.form['username']
+			password = request.form['password']
+			fighter.password = bcrypt.generate_password_hash(password)
+			# fighterCollection.insert(fighter)
+			session['username'] = fighter.name
 			session['logged_in'] = True
-			flash('You were successfully logged in')
-			print session.items()
-			print 'You are logged in as ' + session['username']
-			return redirect (url_for('home'))
+			print "LOGGED IN AS ", session['username']
+			return redirect(url_for('home'))
 		else:
-			error = "Invalid Credentials"
+			error = 'What'
 
-	return render_template('login.html', error=error)
+	return render_template('login.html', error=error, form=form)
 
 
 @app.route('/home')
